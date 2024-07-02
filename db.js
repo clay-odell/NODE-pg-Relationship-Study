72f15035eae97ceb6fc061389d7ccba5f8697a02
Clay O'Dell <clay.odell.music@gmail.com>
2024-07-02 15:21:43 -0500

    fixed the db connection for my psql

--- db.js
+++ db.js
@@ -1,2 +1,14 @@
-/** Database setup for BizTime. */
+const { Client } = require('pg');
+let DB_URI;
+if (process.env.NODE_ENV === 'test') {
+    DB_URI = 'postgresql://postgres:01302@localhost/biztime_test';
+} else {
+    DB_URI = 'postgresql://postgres:01302@localhost/biztime';
+}
 
+let db = new Client({
+    connectionString: DB_URI
+});
+
+db.connect();
+module.exports = db;
