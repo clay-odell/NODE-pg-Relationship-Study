3091480bea6514653146e7dd477612ec24db999d
Clay O'Dell <clay.odell.music@gmail.com>
2024-07-03 12:33:52 -0500

    added get route by id

--- routes/invoices.js
+++ routes/invoices.js
@@ -15,6 +15,24 @@ router.get('/', async function (req, res, next) {
     }
 });
 
+router.get('/:id', async function (req, res, next) {
+    //gets an invoice by id or returns 404
+    try {
+        const { id } = req.params;
+        const invoiceResult = await db.query(
+            `SELECT id, amt, paid, add_date, paid_date, comp_code, companies.name, companies.description FROM invoices
+            JOIN companies ON invoices.comp_code = companies.code
+            WHERE id = $1`,
+            [id]
+        );
+        if (invoiceResult.rows.length === 0) {
+            throw new ExpressError('Invoice Not Found', 404);
+        }
+        return res.status(200).json({invoice: invoiceResult.rows[0]});
+    } catch (err) {
+        return next (err);
+    }
+})
 
 
 
